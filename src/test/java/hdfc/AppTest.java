81deb97604af7009c4e6f04a3904078b70001622
Priya <priyavegiraju123@gmail.com>
2020-10-08 00:20:29 -0400

    first commit

---
+++ src/test/java/hdfc/AppTest.java
@@ -0,0 +1,19 @@
+package hdfc;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
